aadf5c45776fa3a115c387b847e6e9c857505317
Dmytro Kazmiruk <dima.kazmiruk.05@gmail.com>
2023-06-05 21:33:00 +0300

    Add assignment name

--- src/main/java/com/kazmiruk/AssignmentSubmission/domain/Assignment.java
+++ src/main/java/com/kazmiruk/AssignmentSubmission/domain/Assignment.java
@@ -14,6 +14,7 @@ public class Assignment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    private String name;
     private String status;
     private String githubUrl;
     private String branch;
