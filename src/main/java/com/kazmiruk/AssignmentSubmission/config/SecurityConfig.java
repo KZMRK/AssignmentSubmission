83fef554b1013ae72f7fd7425de05ffb4bfdeede
Dmytro Kazmiruk <dima.kazmiruk.05@gmail.com>
2023-06-14 15:04:49 +0300

    Update Spring Security

--- src/main/java/com/kazmiruk/AssignmentSubmission/config/SecurityConfig.java
+++ src/main/java/com/kazmiruk/AssignmentSubmission/config/SecurityConfig.java
@@ -1,60 +1,46 @@
 package com.kazmiruk.AssignmentSubmission.config;
 
+
+import com.kazmiruk.AssignmentSubmission.enums.Role;
 import com.kazmiruk.AssignmentSubmission.filter.JwtFilter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import javax.servlet.http.HttpServletResponse;
-
-@EnableWebSecurity
 @Configuration
+@EnableWebSecurity
 @RequiredArgsConstructor
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-    private final UserDetailsService userDetailsService;
-
-    private final PasswordEncoder passwordEncoder;
+public class SecurityConfig {
 
     private final JwtFilter jwtFilter;
+    private final AuthenticationProvider authenticationProvider;
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http = http.csrf().disable().cors().disable();
-        http = http
+    @Bean
+    protected SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf()
+                .disable()
+                .authorizeHttpRequests()
+                .requestMatchers("/api/auth/**").permitAll()
+                .requestMatchers("/websocket/**").permitAll()
+                .requestMatchers("/api/assignments/**", "/api/comments/**")
+                    .hasAnyAuthority(Role.ROLE_STUDENT.name(), Role.ROLE_CODE_REVIEWER.name())
+                .requestMatchers("/api/users/**").hasAuthority(Role.ROLE_ADMIN.name())
+                .anyRequest()
+                .authenticated()
+                .and()
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and();
+                .and()
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 
-        http = http.exceptionHandling()
-                .authenticationEntryPoint((request, response, e) -> {
-                    response.sendError(HttpServletResponse.SC_UNAUTHORIZED, e.getMessage());
-                })
-                .and();
-
-         http.authorizeRequests()
-                 .antMatchers("/api/auth/**").permitAll()
-                 .antMatchers("/api/assignments/**", "/api/comments").hasAnyAuthority("ROLE_STUDENT", "ROLE_CODE_REVIEWER")
-                 .antMatchers("/api/users/**").hasAuthority("ROLE_ADMIN")
-                 .anyRequest()
-                 .authenticated()
-                 .and()
-                 .formLogin()
-                 .and()
-                 .logout();
-
-         http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
     }
 }
