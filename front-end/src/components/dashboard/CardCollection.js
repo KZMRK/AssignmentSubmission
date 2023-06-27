b955b36920bffb48de959d4e70436d239e36761b
Dmytro Kazmiruk <dima.kazmiruk.05@gmail.com>
2023-06-27 14:47:12 +0300

    Add pagination to dashboards

--- front-end/src/components/dashboard/CardCollection.js
+++ front-end/src/components/dashboard/CardCollection.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { Button, Card, Col, Row } from "react-bootstrap";
+import React, {useEffect, useState} from "react";
+import {Button, Card, Col, Pagination, Row} from "react-bootstrap";
 import StatusBadge from "../statusBadge/StatusBadge";
 
 const CardCollection = (props) => {
@@ -7,10 +7,47 @@ const CardCollection = (props) => {
     const buttonText = props.buttonText;
     const buttonClickedAction = props.buttonClickedAction;
 
+    const [currentPage, setCurrentPage] = useState(1);
+    const recordsPerPage = 8;
+    const lastIndex = currentPage * recordsPerPage;
+    const firstIndex=  lastIndex - recordsPerPage;
+    const records = assignments.slice(firstIndex, lastIndex);
+    const npage = Math.ceil(assignments.length / recordsPerPage);
+    const [pagItems, setPagItems] = useState([]);
+
+    useEffect(() => {
+        const newPagItems = [];
+        newPagItems.push(<Pagination.Prev
+            onClick={() => {
+                if (currentPage > 1) {
+                    setCurrentPage(currentPage - 1);
+                }
+            }}
+        />);
+        for (let number = 1; number <= npage; number++) {
+            newPagItems.push(
+                <Pagination.Item key={number}
+                                 active={number === currentPage}
+                                 onClick={() => {
+                                     setCurrentPage(number)
+                                 }}
+                >
+                    {number}
+                </Pagination.Item>,
+            )
+        }
+        newPagItems.push(<Pagination.Next onClick={() => {
+            if (currentPage < npage) {
+                setCurrentPage(currentPage + 1);
+            }
+        }} />);
+        setPagItems(newPagItems);
+    }, [currentPage]);
+
     return (
         <div>
             <Row xs={1} md={2} lg={3} xl={4} className="g-2">
-                {assignments.map((assignment) => (
+                {records.map((assignment) => (
                     <Col
                         className="d-flex justify-content-center"
                         key={assignment.id}
@@ -40,6 +77,9 @@ const CardCollection = (props) => {
                     </Col>
                 ))}
             </Row>
+            <Pagination className="mt-4 justify-content-center">
+                {pagItems}
+            </Pagination>
         </div>
     );
 };
